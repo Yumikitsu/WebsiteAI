047b3da2b6602720b4b609510a3fd5f276bac62e
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-10-24 13:34:15 +0200

    AI Generated Website (Don't get seasick)

---
+++ workshop.wwwapi/Controller/CarController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CarsController : ControllerBase
+{
+    private static List<Car> cars = new List<Car>
+    {
+        new Car { Id = 1, Make = "Toyota", Model = "Corolla", Year = 2020 },
+        new Car { Id = 2, Make = "Honda", Model = "Civic", Year = 2019 },
+        new Car { Id = 3, Make = "Ford", Model = "Focus", Year = 2018 }
+    };
+
+    [HttpGet]
+    public IEnumerable<Car> Get()
+    {
+        return cars;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Car car)
+    {
+        car.Id = cars.Max(c => c.Id) + 1;
+        cars.Add(car);
+        return CreatedAtAction(nameof(Get), new { id = car.Id }, car);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] Car updatedCar)
+    {
+        var car = cars.FirstOrDefault(c => c.Id == id);
+        if (car == null)
+        {
+            return NotFound();
+        }
+        car.Make = updatedCar.Make;
+        car.Model = updatedCar.Model;
+        car.Year = updatedCar.Year;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var car = cars.FirstOrDefault(c => c.Id == id);
+        if (car == null)
+        {
+            return NotFound();
+        }
+        cars.Remove(car);
+        return NoContent();
+    }
+}
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
---
+++ workshop.wwwapi/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cars API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cars API v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
